8d09df184aea0e44d91a7fa5b3e2f878348eecc8
Jose G Martinez <jgermanmm@gmail.com>
2022-09-27 08:35:31 -0300

    FIn tarea ajax

--- 04-Ajax/homework/index.js
+++ 04-Ajax/homework/index.js
@@ -1,6 +1,4 @@
 // ejercicio 1
-
-
 $('#boton').click(function(){
     $.get('http://localhost:5000/amigos', data => {
         let listaAmigos = document.querySelector("#lista");
@@ -9,9 +7,38 @@ $('#boton').click(function(){
 
         for (let i = 0; i < data.length; i++) {
             let li = document.createElement('li');
-            li.textContent = `Nombre: ${data[i].name}, edad: ${data[i].age}`;
+            li.textContent = `Id: ${data[i].id} ---> Nombre: ${data[i].name}, edad: ${data[i].age}`;
             listaAmigos.appendChild(li);
         }
 
     }); 
   });
+
+  //ejercicio 2
+$("#search").click(() => {
+
+    let input = document.querySelector("#input")
+
+    $.get(`http://localhost:5000/amigos/${input.value}`, data => {
+        console.log(data);
+
+        let span = document.querySelector("#amigo");
+        span.textContent = `Tu amigo con el id: ${input.value} es ${data.name}`
+    })
+})
+
+//ejercicio 3
+$ ("#delete").click(() => {
+
+    let inputDelete = document.querySelector("#inputDelete")
+
+    $.ajax({
+        url:`http://localhost:5000/amigos/${inputDelete.value}`,
+        type: 'DELETE',
+        success: function (result){
+            $("#success").text(`Tu amigo 👦 con el id ${inputDelete.value} fue eliminado`)
+
+        }
+    });
+
+})
