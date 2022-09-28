182682899a27dd94c33dad61b2e595e198dad6b2
Jose G Martinez <jgermanmm@gmail.com>
2022-09-28 11:48:58 -0300

    React intro HW

--- 06-React-Intro/homework/src/components/Cards.jsx
+++ 06-React-Intro/homework/src/components/Cards.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import Card from "./Card";
 
-export default function Cards(props) {
+export default function Cards({cities}) {
   // acá va tu código
   // tip, podés usar un map
+  if(!cities) {
+    return <h1>No hay ciudades disponibles</h1>
+}
+  return (
+    <div>
+      {
+        cities && cities.map( city => (
+          <Card
+          key = {city.id}
+          max = {city.main.temp_max}
+          min = {city.main.temp_min}
+          name = {city.name}
+          img = {city.weather[0].icon}
+          onClose = {() => alert(city.name)}
+          />
+        ))
+      }
+
+
+
+    </div>
+  )
+
 
   
   return <div>Cards Component</div>
